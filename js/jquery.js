6f8ac0302dc608242223a73250bbb97441e86083
Aaron Guyett <aaronguyett@yahoo.com>
2020-02-09 16:07:26 -0800

    Contact and Table
    Added jQuery table that toggles on and off. Created a contact button that moves with the page, and began to gather images for projects.

---
+++ js/jquery.js
@@ -0,0 +1,17 @@
+/* Show and hide the table */
+$('.skills').click(function(){
+  $("#menu").toggle("slow", function(){
+  });
+});
+
+/* Floating Contact Button */
+$(window).scroll(function() {
+    var winScrollTop = $(window).scrollTop();
+    var winHeight = $(window).height();
+    var floaterHeight = $('.contact').outerHeight(true);
+    //true so the function takes margins into account
+    var fromBottom = 20;
+
+    var top = winScrollTop + winHeight - floaterHeight - fromBottom;
+    $('.contact').css({'top': top + 'px'});
+});
